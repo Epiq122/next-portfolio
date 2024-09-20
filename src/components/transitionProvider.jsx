8ba256aa1062d98abab3006b53003ed61142df15
Robert Gleason <l2ob122@gmail.com>
2024-09-20 14:52:24 -0700

    created page transition animation

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -8,7 +8,7 @@ const TransitionProvider = ({ children }) => {
   const pathname = usePathname();
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode='wait'>
       <div
         key={pathname}
         className='w-screen h-screen bg-gradient-to-b from-blue-100 to-red-200'
@@ -18,6 +18,20 @@ const TransitionProvider = ({ children }) => {
           animate={{ height: '0vh' }}
           exit={{ height: '140vh' }}
           transition={{ duration: 0.5, ease: 'easeOut' }}
+        />
+        <motion.div
+          className='fixed m-auto top-0 left-0 right-0 bottom-0 text-white text-8xl cursor-default z-50 w-fit h-fit'
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+        >
+          {pathname.substring(1)}
+        </motion.div>
+        <motion.div
+          className='h-screen w-screen fixed bg-black rounded-b-[100px] z-40'
+          initial={{ height: '140vh' }}
+          animate={{ height: '0vh', transition: { delay: 0.5 } }}
         />
         <div className='h-24'>
           <Navbar />
