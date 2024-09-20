1bcd0c681c5765cf4f7c083ba56e94f73baf6d34
Robert Gleason <l2ob122@gmail.com>
2024-09-20 12:10:00 -0700

    created small hamburger for the nav, added links to for the nav and socials

---
+++ src/components/navbar.jsx
@@ -0,0 +1,71 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+const menuItems = [
+  { url: '/', title: 'Home' },
+  { url: '/about', title: 'About' },
+  { url: '/portfolio', title: 'Portfolio' },
+  { url: '/contact', title: 'Contact' },
+];
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+      {/* LINKS */}
+      <div className='hidden md:flex gap-4 w-1/3'>
+        {menuItems.map((item) => (
+          <Link key={item.title} href={item.url}>
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className='md:hidden lg:flex w-1/3 justify-center'>
+        <Link
+          href='/'
+          className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'
+        >
+          <span className='text-white mr-2'>Robert</span>
+          <span className='w-13 h-8 rounded bg-white text-black flex items-center justify-center'>
+            Gleason
+          </span>
+        </Link>
+      </div>
+      {/* SOCIALS */}
+      <div className='hidden md:flex gap-4 w-1/3 '>
+        <Link href='https://github.com/epiq122'>
+          <Image src='/github.png' alt='github' width={24} height={24} />
+        </Link>
+        <Link href='https://www.linkedin.com/in/robert-gleason-222b30178/'>
+          <Image src='/linkedin.png' alt='linkedin' width={24} height={24} />
+        </Link>
+      </div>
+      {/* RESPONSIVE MENU */}
+      <div className='md:hidden'>
+        {/* Menu button */}
+        <button
+          className='w-10 h-8 flex flex-col justify-between z-50 relative'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className='w-10 h-1 bg-white rounded-md' />
+          <div className='w-10 h-1 bg-white rounded-md' />
+          <div className='w-10 h-1 bg-white rounded-md' />
+        </button>
+        {/* Menu items */}
+        {isOpen && (
+          <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+            {menuItems.map((item) => (
+              <Link key={item.title} href={item.url}>
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Navbar;
