e3eb4614b1127e576afc16c1db1117366102c62c
Robert Gleason <l2ob122@gmail.com>
2024-10-12 19:35:16 -0700

    updated bio

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -27,8 +27,14 @@ const About = () => {
           <div className='flex flex-col gap-12 justify-center'>
             <h1 className='font-bold text-2xl'>BIOGRAPHY</h1>
             <p className='text-lg'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quos.
+              I am Robert Gleason (Rob), a developer living in Vancouver,Bc,
+              making a transition from the trades world to web development,
+              Coming from a trades background running a team of workers, helping
+              customers and creating solutions to better ther life or make there
+              vision come to life. I have always wanted to do something with
+              computers and now I want to apply the same need to build something
+              In the digital world and help businesses with modern web
+              solutions.
             </p>
 
             <motion.svg
@@ -145,107 +151,110 @@ const About = () => {
               EXPERIENCE
             </motion.h1>
             {/* EXPERIENCE LIST */}
-            <motion.div
+            {/* <motion.div
               initial={{ x: '-300px' }}
               animate={isExperienceRefInView ? { x: 0 } : { x: {} }}
             >
               {/* EXPERIENCE ITEM */}
-              <div className='flex justify-between h-48'>
+            {/* <div className='flex justify-between h-48'>
                 {/* left */}
-                <div className='w-1/3'>
+            {/* <div className='w-1/3'>
                   {/* job title */}
-                  <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
+            {/* <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
                     TODO : ADD JOB
                   </div>
                   {/* job description */}
-                  <div className='p-3 text-sm italic'>
+            {/* <div className='p-3 text-sm italic'>
                     TODO : ADD JOB DESCRIPTION
                   </div>
                   {/* JOB DATE */}
-                  <div className='p-3 text-sm text-red-400 font-semibold'>
+            {/* <div className='p-3 text-sm text-red-400 font-semibold'>
                     TODO : ADD JOB DATE
                   </div>
                   {/* Job Company */}
-                  <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
+            {/* <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
                     TODO : ADD JOB COMPANY
                   </div>
                 </div>
                 {/* center */}
-                <div className='w-1/6'>
+            {/* <div className='w-1/6'>
                   {/* line */}
-                  <div className='w-1 h-full bg-gray-600 rounded relative'>
+            {/* <div className='w-1 h-full bg-gray-600 rounded relative'>
                     {/* cirle */}
-                    <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
+            {/* <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
                   </div>
                 </div>
                 {/* right */}
-                <div className='w-1/3'></div>
+            {/* <div className='w-1/3'></div>
               </div>
               {/* EXPERIENCE ITEM */}
-              <div className='flex justify-between h-48'>
+            {/* <div className='flex justify-between h-48'>
                 {/* left */}
-                <div className='w-1/3'></div>
+            {/* <div className='w-1/3'></div>
                 {/* center */}
-                <div className='w-1/6'>
+            {/* <div className='w-1/6'>
                   {/* line */}
-                  <div className='w-1 h-full bg-gray-600 rounded relative'>
+            {/* <div className='w-1 h-full bg-gray-600 rounded relative'>
                     {/* cirle */}
-                    <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
+            {/* <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
                   </div>
                 </div>
                 {/* right */}
-                <div className='w-1/3'>
+            {/* <div className='w-1/3'>
                   {/* job title */}
-                  <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
+            {/* <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
                     TODO : ADD JOB
                   </div>
                   {/* job description */}
-                  <div className='p-3 text-sm italic'>
+            {/* <div className='p-3 text-sm italic'>
                     TODO : ADD JOB DESCRIPTION
                   </div>
                   {/* JOB DATE */}
-                  <div className='p-3 text-sm text-red-400 font-semibold'>
+            {/* <div className='p-3 text-sm text-red-400 font-semibold'>
                     TODO : ADD JOB DATE
                   </div>
                   {/* Job Company */}
-                  <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
+            {/* <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
                     TODO : ADD JOB COMPANY
                   </div>
                 </div>
               </div>
               {/* EXPERIENCE ITEM */}
-              <div className='flex justify-between h-48'>
+            {/* <div className='flex justify-between h-48'>
                 {/* left */}
-                <div className='w-1/3'>
+            {/* <div className='w-1/3'>
                   {/* job title */}
-                  <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
+            {/* <div className='bg-white p-3 font-semibold rounded-b-lg rounded-s-lg'>
                     TODO : ADD JOB
                   </div>
                   {/* job description */}
-                  <div className='p-3 text-sm italic'>
+            {/* <div className='p-3 text-sm italic'>
                     TODO : ADD JOB DESCRIPTION
                   </div>
                   {/* JOB DATE */}
-                  <div className='p-3 text-sm text-red-400 font-semibold'>
+            {/* <div className='p-3 text-sm text-red-400 font-semibold'>
                     TODO : ADD JOB DATE
                   </div>
                   {/* Job Company */}
-                  <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
+            {/* <div className='p-1 rounded bg-white text-sm font-semibold w-fit'>
                     TODO : ADD JOB COMPANY
                   </div>
                 </div>
                 {/* center */}
-                <div className='w-1/6'>
+            {/* <div className='w-1/6'>
                   {/* line */}
-                  <div className='w-1 h-full bg-gray-600 rounded relative'>
+            {/* <div className='w-1 h-full bg-gray-600 rounded relative'>
                     {/* cirle */}
-                    <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
+            {/* <div className='absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2'></div>
                   </div>
                 </div>
                 {/* right */}
-                <div className='w-1/3'></div>
+            {/* <div className='w-1/3'></div>
               </div>
-            </motion.div>
+            </motion.div> */}
+            <div className='text-center text-xl font-bold text-red-500'>
+              Under Construction
+            </div>
           </div>
         </div>
         {/* SVG CONTAINER */}
