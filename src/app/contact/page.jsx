adc8ce6f0a7c346b92c8e8887e6fa2db2e195579
Robert Gleason <l2ob122@gmail.com>
2024-10-31 19:02:07 -0700

    changed variable for emailjs

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -20,7 +20,7 @@ const ContactPage = () => {
         process.env.NEXT_PUBLIC_SERVICE_ID,
         process.env.NEXT_PUBLIC_TEMPLATE_ID,
         form.current,
-        process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        process.env.NEXT_PUBLIC_KEY,
       )
       .then(
         () => {
